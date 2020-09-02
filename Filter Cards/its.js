7bb324dd166ac96f458914e3b1c46644c0f3992d
kamal Jyotwal <kkjyotwal100@gmail.com>
2020-09-02 20:24:54 +0530

    Rewrote JS for filter card

--- Filter Cards/its.js
+++ Filter Cards/its.js
@@ -1,38 +1,12 @@
-filterSelection("all");
-
-function filterSelection(para) {
+function filterSelection(params) {
   var card = document.getElementsByClassName("imgDiv");
-  if (para == "all") {
-    para = "";
-  }
 
   for (let i = 0; i < card.length; i++) {
-    removeClass(card[i], "show");
-    if (card[i].className.indexOf(para) > -1) {
-      addClass(card[i], "show");
+    card[i].style.display = "none";
+    if (card[i].classList.contains(params)) {
+
+      card[i].classList.add('animation');
+      card[i].style.display = "block"
     }
   }
-}
-
-function addClass(elem, clname) {
-  var el = elem.className.split(" ");
-  var cl = clname.split(" ");
-
-  for (let j = 0; j < cl.length; j++) {
-    if (el.indexOf(cl[j]) == -1) {
-      elem.className += " " + cl;
-    }
-  }
-}
-
-function removeClass(elem, clname) {
-  var el = elem.className.split(" ");
-  var cl = clname.split(" ");
-
-  for (let j = 0; j < cl.length; j++) {
-    while (el.indexOf(cl[j]) > -1) {
-      el.splice(el.indexOf(cl[j], 1));
-    }
-  }
-  elem.className = el.join(" ");
 }
